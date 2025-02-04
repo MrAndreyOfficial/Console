8b0f65739be9157589ae314bf4697790250a4b0a
MrAndreyOfficial <mrandreyofficial@gmail.com>
2025-02-04 17:42:26 +0500

    Array from keyboard

---
+++ C#/Array from keyboard/Program.cs
@@ -0,0 +1,41 @@
+﻿WriteTip("Press CTRL + C to exit", true);
+
+while (true)
+{
+    var length = Input("Enter the length of the array: ");
+
+    if (int.TryParse(length, out int arrayLength))
+    {
+        string[] array = new string[arrayLength];
+
+        for (int i = 0; i < arrayLength; i++)
+            array[i] = Input($"Enter element by index {i}: ")!;
+
+        foreach (var element in array)
+            WriteTip(element?.ToString(), true);
+
+        continue;
+    }
+
+    WriteTip("Enter the number!", true);
+}
+
+static void WriteTip(string? tip = "", bool useNewLine = false)
+{
+    if (useNewLine)
+    {
+        Console.WriteLine(tip);
+        return;
+    }
+
+    Console.Write(tip);
+}
+
+static string? Input(string tip = "", bool useNewLine = false)
+{
+    WriteTip(tip);
+
+    var input = Console.ReadLine();
+
+    return input;
+}
---
+++ C#/Array from keyboard/Readme.md
@@ -0,0 +1,6 @@
+Mini app made in [C#](https://dotnet.microsoft.com/en-us/languages/csharp/)
+
+In this app:
+1. Enter length of array
+2. Enter elements in array
+3. Write elements from array
